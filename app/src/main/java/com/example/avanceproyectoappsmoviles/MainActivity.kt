1e01ff998d203e0f25fca492bf8ccc7a82c1a6d5
Alan <Traxcomgarza@outlook.com>
2025-08-30 19:02:22 -0600

    Cambios Para notificaciones

--- app/src/main/java/com/example/avanceproyectoappsmoviles/MainActivity.kt
+++ app/src/main/java/com/example/avanceproyectoappsmoviles/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.avanceproyectoappsmoviles
 
+import android.R.attr.title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,8 +23,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -34,13 +41,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.avanceproyectoappsmoviles.ui.theme.AvanceProyectoAppsMovilesTheme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AvanceProyectoAppsMovilesTheme {
+            AvanceProyectoAppsMovilesTheme  {
+                    SintomasCrearMostrar(
+                        sintomasViewModel = SintomasViewModel()
+                    )
+
 
                 }
             }
@@ -53,11 +65,15 @@ data class SintomasData(val text: String)
 @Composable
 fun SintomasCrearMostrar(modifier: Modifier = Modifier, sintomasViewModel: SintomasViewModel ){
     var input by rememberSaveable { mutableStateOf("") }
-
     Column (
         modifier = Modifier
+            .fillMaxSize()
             .padding(30.dp)
-    ){
+
+
+    )
+    {
+
         TextField(
             value = input,
             onValueChange = {input = it},
@@ -65,12 +81,16 @@ fun SintomasCrearMostrar(modifier: Modifier = Modifier, sintomasViewModel: Sinto
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.size(10.dp))
-        Row {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
             Button(
                 onClick = {
                     sintomasViewModel.addSintomas(input)
                     input = ""
                 },
+
                 enabled = input.isNotEmpty()
             ) {
                 Text("Nuevo Sintoma")
@@ -100,5 +120,17 @@ fun SintomasCrearMostrar(modifier: Modifier = Modifier, sintomasViewModel: Sinto
         }
 
     }
+}
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun Preview(){
+    AvanceProyectoAppsMovilesTheme {
+        SintomasCrearMostrar(
+            sintomasViewModel = SintomasViewModel()
+        )
+    }
+
+
 }
 
